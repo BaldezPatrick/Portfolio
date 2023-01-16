3d9172603efeaf9d3d4152e0068f8ea99e8df4a7
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-16 17:33:09 -0300

    Index.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact, faHtml5, faCss3, faJsSquare, faGitAlt, faPython } from '@fortawesome/free-brands-svg-icons'
+import {
+  faReact,
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faGitAlt,
+  faPython,
+} from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
@@ -10,7 +17,7 @@ const About = () => {
 
   useEffect(() => {
     setTimeout(() => {
-        setLetterClass('text-animate-hover')
+      setLetterClass('text-animate-hover')
     }, 1500)
   }, [])
   return (
@@ -19,46 +26,55 @@ const About = () => {
         <div className="text-zone">
           <h1>
             <AnimatedLetters
-            letterClass={letterClass}
+              letterClass={letterClass}
               strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
               idx={15}
             />
           </h1>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, dicta? Esse, modi corrupti sapiente beatae eum ad est maxime voluptatum autem veritatis culpa quos qui molestiae ipsum dolore repudiandae facilis!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim,
+            dicta? Esse, modi corrupti sapiente beatae eum ad est maxime
+            voluptatum autem veritatis culpa quos qui molestiae ipsum dolore
+            repudiandae facilis!
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto velit inventore quaerat eveniet sint. Possimus corrupti eligendi optio, ipsum nisi tempora eos explicabo vitae, eaque error suscipit incidunt enim tenetur.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            velit inventore quaerat eveniet sint. Possimus corrupti eligendi
+            optio, ipsum nisi tempora eos explicabo vitae, eaque error suscipit
+            incidunt enim tenetur.
           </p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo veniam ducimus, a fuga praesentium dicta. Officia corporis corrupti repellendus amet minus ipsa atque porro tempore cupiditate. Architecto fuga cupiditate officia.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            veniam ducimus, a fuga praesentium dicta. Officia corporis corrupti
+            repellendus amet minus ipsa atque porro tempore cupiditate.
+            Architecto fuga cupiditate officia.
           </p>
         </div>
 
-        <div className='stage-cube-cont'>
-          <div className='cubespinner'>
-            <div className='face1'>
-              <FontAwesomeIcon icon={faReact} color="#28A4D9"/>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#28A4D9" />
             </div>
-            <div className='face2'>
-              <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
-            <div className='face3'>
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
             </div>
-            <div className='face4'>
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
             </div>
-            <div className='face5'>
-              <FontAwesomeIcon icon={faGitAlt} color="#F06529"/>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#F06529" />
             </div>
-            <div className='face6'>
-              <FontAwesomeIcon icon={faPython} color="#EFD81D"/>
+            <div className="face6">
+              <FontAwesomeIcon icon={faPython} color="#EFD81D" />
             </div>
           </div>
         </div>
       </div>
-      <Loader type='pacman' />
+      <Loader type="pacman" />
     </>
   )
 }
