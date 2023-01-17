f91e0973548738c2abef0df65f25e6bce5c5403b
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-17 17:19:06 -0300

    App changes.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <main className='main'>
         <Home />
+        <About />
+        <Skills />
       </main>
     </>
   )
