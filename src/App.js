78f97dea1d73130ab4ab26a6487fe29289f62108
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-17 15:39:42 -0300

    Import to use in App component.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
       <Header />
+      <main className='main'>
+        <Home />
+      </main>
     </>
   )
 }
