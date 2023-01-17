9e48129c7eba8d416df4d482d57fbfee8139f2aa
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-17 14:08:00 -0300

    New App.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react'
-import './App.scss'
+import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Projects from './components/Projects'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-        </Route>
-      </Routes>
+      <Header />
     </>
   )
 }
