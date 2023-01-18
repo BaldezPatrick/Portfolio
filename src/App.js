32964347b552b7e61be04c48aa16a7cad7a0fe15
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-18 18:57:17 -0300

    Imported Header's component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Skills from './components/Skills/Skills';
 
 function App() {
   return (
     <>
       <Header />
       <main className='main'>
-        <Home />
-        <About />
-        <Skills />
+
       </main>
     </>
   )
