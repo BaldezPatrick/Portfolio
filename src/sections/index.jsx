977cf97ad1216619c85360ab8dcdb194e3a26dc2
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-26 17:23:11 -0300

    Place to organize the import and export of sections.

---
+++ src/sections/index.jsx
@@ -0,0 +1,21 @@
+import NavBar from '../sections/navbar/NavBar'
+import Header from '../sections/header/Header'
+import About from '../sections/about/About'
+import Skills from '../sections/skills/Skills'
+import Portfolio from '../sections/portfolio/Portfolio'
+import FAQs from '../sections/faqs/FAQs'
+import Contact from '../sections/contact/Contact'
+import Footer from '../sections/footer/Footer'
+import FloatingNav from '../sections/floating-nav/FloatingNav'
+
+export {
+  NavBar,
+  Header,
+  About,
+  Skills,
+  Portfolio,
+  FAQs,
+  Contact,
+  Footer,
+  FloatingNav,
+}
