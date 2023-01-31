49cba25676ee506acca27995d7a5dd52785a3ccc
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-31 18:56:11 -0300

    feat(Header): Second Header's version.

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import './header.css'
+import data from './data'
 
 const Header = () => {
   return (
     <section id="header">
       <div className="container header-container">
         <div className="header-profile">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum architecto voluptatum laboriosam quasi illo numquam rerum? Error, veritatis non!
+        </div>
           <h3>Patrick Baldez</h3>
           <p>
-            You are a click away from bulding your dream website. Send me the details of your project for a modern, responsive, highly performat website today!
+            You are a click away from bulding your dream website. Send me the
+            details of your project for a modern, responsive, highly performat
+            website today!
           </p>
           <div className="header-cta">
-            <a href="#contact">Let's Talk</a>
-            <a href="#portfolio">My work</a>
+            <a href="#contact" className='btn primary'>Let's Talk</a>
+            <a href="#portfolio" className='btn light'>My work</a>
           </div>
           <div className="header-socials">
-            
+            {data.map((item) => (
+              <a
+                key={item.id}
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            ))}
           </div>
-        </div>
       </div>
     </section>
   )
