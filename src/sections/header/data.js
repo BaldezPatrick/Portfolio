38da446fd97361f792c014139a5ffa1395ac33a4
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-31 18:55:25 -0300

    feat(Header): Data for contacts that will appear in the Header component.

---
+++ src/sections/header/data.js
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const data = [
+    {id: 1, link: 'https://github.com', icon: <FontAwesomeIcon icon={faGithub} />},
+    {id: 2, link: 'https://linkedin.com', icon: <FontAwesomeIcon icon={faLinkedin}/>}
+]
+
+export default data;
