46e253d7a1696d88554ac435f58fd5fa44c15a94
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-30 21:54:06 -0300

    feat(NavBar): NavBar component in progress.

--- src/sections/navbar/NavBar.jsx
+++ src/sections/navbar/NavBar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './navBar.css'
+import data from './data'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPalette } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
   return (
     <nav>
-        <h2>NavBar</h2>
+      <div className="container nav-container">
+        <a href="index.html" className="nav-logo">
+          PB
+        </a>
+        <ul className="nav-menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme-icon">
+          <FontAwesomeIcon icon={faPalette} />
+        </button>
+      </div>
     </nav>
   )
 }
