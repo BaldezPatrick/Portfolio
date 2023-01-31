968590a2ed353ae1680c158ccb9b77444b1eabf5
BaldezPatrick <patrickbaldex@gmail.com>
2023-01-30 22:18:26 -0300

    feat(Header): Header start.

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -4,7 +4,21 @@ import './header.css'
 const Header = () => {
   return (
     <section id="header">
-        <h2>Header</h2>
+      <div className="container header-container">
+        <div className="header-profile">
+          <h3>Patrick Baldez</h3>
+          <p>
+            You are a click away from bulding your dream website. Send me the details of your project for a modern, responsive, highly performat website today!
+          </p>
+          <div className="header-cta">
+            <a href="#contact">Let's Talk</a>
+            <a href="#portfolio">My work</a>
+          </div>
+          <div className="header-socials">
+            
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
