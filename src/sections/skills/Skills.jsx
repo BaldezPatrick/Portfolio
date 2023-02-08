38fbe26f3c50cd1fb97ba33969e8fdbcbcdc5e05
BaldezPatrick <patrickbaldex@gmail.com>
2023-02-07 21:12:43 -0300

    feat(Skills): First part of Skills.

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './skills.css';
+import Card from '../../components/Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJs } from '@fortawesome/free-brands-svg-icons';
 
 const Skills = () => {
   return (
     <section id="skills">
-      <h2>Skills</h2>
+      <h2>My Skills</h2>
+      <div className="container skills-container">
+        <Card className="service light">
+            <div className="service-icon">
+                <FontAwesomeIcon icon={faJs
+                } />
+            </div>
+        </Card>
+      </div>
     </section>
   )
 }
