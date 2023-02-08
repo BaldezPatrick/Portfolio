3e829159815932c24cb8fbd96135188e307078ab
BaldezPatrick <patrickbaldex@gmail.com>
2023-02-07 21:24:56 -0300

    feat(Skills): Infos that will be used in Skills component.

---
+++ src/sections/skills/data.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faJs, faHtml5, faCss3, faReact, faNode, faPython, faGit } from "@fortawesome/free-brands-svg-icons"
+const data = [
+    {id: 1, icon: <FontAwesomeIcon icon={faHtml5}/>},
+    {id: 2, icon: <FontAwesomeIcon icon={faCss3}/>},
+    {id: 3, icon: <FontAwesomeIcon icon={faJs}/>},
+    {id: 4, icon: <FontAwesomeIcon icon={faReact} />},
+    {id: 5, icon: <FontAwesomeIcon icon={faNode} />},
+    {id: 6, icon: <FontAwesomeIcon icon={faPython} />},
+    {id: 7, icon: <FontAwesomeIcon icon={faGit} />}
+]
+
+export default data;
