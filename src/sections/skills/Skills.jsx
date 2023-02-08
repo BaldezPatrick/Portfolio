a1044881d9e155b00811abef9aad2d86111d13bd
BaldezPatrick <patrickbaldex@gmail.com>
2023-02-07 21:25:31 -0300

    feat(Skills): New component structure.

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -3,18 +3,22 @@ import './skills.css';
 import Card from '../../components/Card'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faJs } from '@fortawesome/free-brands-svg-icons';
+import dataSkills from './data'
 
 const Skills = () => {
   return (
     <section id="skills">
       <h2>My Skills</h2>
       <div className="container skills-container">
-        <Card className="service light">
-            <div className="service-icon">
-                <FontAwesomeIcon icon={faJs
-                } />
-            </div>
-        </Card>
+        {
+          dataSkills.map((item) => (
+            <Card key={item.id} className="service light">
+              <div className="service-icon">
+                {item.icon}
+              </div>
+            </Card>
+          ))
+        }
       </div>
     </section>
   )
