7f6cd2d9ab459357131e64a2a0bc9c2385059489
BaldezPatrick <patrickbaldex@gmail.com>
2023-02-08 20:25:17 -0300

    feat(Portfolio): Add examples of info to use in the Portfolio's component.

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        category: "frontend",
+        title: "A title for project (Frontend)",
+        description: "ahushduashduahsduahsudhasudhasudhaushduahduahsduahsduahsudhausdhaushduashduahsudhasudhaushduashdauhda",
+        demo: "https://linkedin.com",
+        github: "https://github.com"
+    },
+    {
+        id: 2,
+        category: "backend",
+        title: "A title for project A (Backend)",
+        description: "ahushduashduahsduahsudhasudhasudhaushduahduahsduahsduahsudhausdhaushduashduahsudhasudhaushduashdauhda",
+        demo: "https://linkedin.com",
+        github: "https://github.com"
+    },
+    {
+        id: 3,
+        category: "frontend",
+        title: "A title for project B (Frontend)",
+        description: "ahushduashduahsduahsudhasudhasudhaushduahduahsduahsduahsudhausdhaushduashduahsudhasudhaushduashdauhda",
+        demo: "https://linkedin.com",
+        github: "https://github.com"
+    },
+    {
+        id: 4,
+        category: "backend",
+        title: "A title for project C (Backend)",
+        description: "ahushduashduahsduahsudhasudhasudhaushduahduahsduahsduahsudhausdhaushduashduahsudhasudhaushduashdauhda",
+        demo: "https://linkedin.com",
+        github: "https://github.com"
+    }
+]
