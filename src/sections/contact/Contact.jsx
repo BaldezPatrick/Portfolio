2a047ffb50a356f0302fa6765066a8fa93917f09
BaldezPatrick <patrickbaldex@gmail.com>
2023-02-10 16:06:45 -0300

    feat(Contact): Structure of the contact component.

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './contact.css'
+import React from 'react';
+import './contact.css';
+import dataContacts from './data';
 
 const Contact = () => {
   return (
     <section id="contact">
-        <h2>Contacts</h2>
+        <h2>Contact</h2>
+        <p>
+          Shoot me a message via any of the contacts bellow
+        </p>
+        <div className="container contact-container">
+            {
+              dataContacts.map((contact) => {
+                <a key={contact.id} href={contact.link} target="_blank" rel="noopener norferrer">{contact.icon}</a>
+              })
+            }
+        </div>
     </section>
   )
 }
