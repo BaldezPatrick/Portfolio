d219afbc745c3010edf39ba2d32569b04483c3e7
BaldezPatrick <patrickbaldex@gmail.com>
2023-03-01 10:51:58 -0300

    feat(Footer): Footer component.

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { links, socials } from './data'
 import './footer.css'
 
 const Footer = () => {
   return (
     <footer>
-        <h2>Footer</h2>
+      <div className="container footer-container">
+        <ul className="nav-menu">
+          {links.map((link) => (
+            <li key={link.id}>
+              <a href={link.link}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="footer-socials">
+          {
+            socials.map(social => <a key={social.id} href={social.link} target='_blank' rel='noopener noreferrer'>
+              {social.icon}
+            </a>)
+          }
+        </div>
+      </div>
     </footer>
   )
 }
