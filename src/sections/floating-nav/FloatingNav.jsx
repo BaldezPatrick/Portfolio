8c7afbc3a20a6a332d9a48b078f4709b9fe1b93f
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-18 14:44:10 -0300

    Style nav floating.

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './floating-nav.css'
+import Nav from './Nav'
+import data from './data'
+import Scrollspy from 'react-scrollspy'
 
 const FloatingNav = () => {
   return (
-    <section id="floating__nav">
-        
-    </section>
+    <ul id="floating__nav">
+        <Scrollspy offset={-400} className='scroll' items={['header', 'about', 'skills', 'portfolio', 'contact']} currentClassName='active'>
+          {
+            data.map(item => <Nav key={item.id} item={item} />)
+          }
+        </Scrollspy>
+    </ul>
   )
 }
 
